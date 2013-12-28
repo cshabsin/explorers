d8df9c3192fcef11fcebace128e80d80e5820fee
Chris Shabsin <cshabsin@gmail.com>
2013-12-28 15:43:38 -0500

    work in progress

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 var mapdiv = d3.select("#map");
 
-settings_div = mapdiv.append("div");
+settings_div = d3.select("#settings");
 checkbox = settings_div.append("input").attr("type", "checkbox").attr("class", "map-setting");
-settings_div.append("label").text("The path of the Spiny Rat").attr("style", "color:white");
+settings_div.append("label").text("The path of the Spiny Rat").style("color", "white");
 
 checkbox.on("change", function() {
     isChecked = this.checked;
@@ -13,9 +13,38 @@ checkbox.on("change", function() {
     }
 });
 
+var drag = d3.behavior.drag();
+drag.on("drag", function(d, i) {
+    d.x += d3.event.dx;
+    d.y += d3.event.dy;
+    d3.select(this).attr("transform", function(d,i) {
+	return "translate(" + [d.x, d.y] + ")";
+    });
+});
+mapdiv.__data__ = {};
+mapdiv.__data__.x = 0;
+mapdiv.__data__.y = -500;
+//mapdiv.attr("transform", "translate(" + [mapdiv.__data__.x, mapdiv.__data__.y] + ")");
+//mapdiv.call(drag);
+
 a = createMap(mapdiv, hexArray);
 svg = a[0];
 map = a[1];
+
+function addArrowDef(svg) {
+    svg.append("defs").append("marker").
+	attr("id", "head").
+	attr("orient", "auto").
+	attr("markerWidth", 13).
+	attr("markerHeight", 13).
+	attr("refx", 2).
+	attr("refy", 6).
+	append("path").
+	attr("d", "M2,2 L2,11 L10,6 L2,2").
+	attr("fill", "red");
+}
+
+addArrowDef(svg);
 
 function pointRel(hex, offset) {
     if (hex.center == null) {
@@ -41,7 +70,7 @@ function shipTraversal(sourceHex, sourceOffset, destinationHex, destinationOffse
 }
 
 spinyRatPath = [];
-spinyRatPath.push(shipTraversal(hexes.Khida, dir.W, hexes.GimiKuuid, dir.SE));
+spinyRatPath.push(shipTraversal(hexes.Khida, dir.SW, hexes.GimiKuuid, dir.SE));
 spinyRatPath.push(shipTraversal(hexes.GimiKuuid, dir.SW, hexes.Vlair, dir.SE));
 spinyRatPath.push(shipTraversal(hexes.Vlair, dir.SW, hexes.Uure, dir.SE));
 spinyRatPath.push(shipTraversal(hexes.Uure, dir.SW, hexes.Forquee, dir.SE));
@@ -68,4 +97,5 @@ for (var i = 0; i<spinyRatPath.length; i++) {
     spinyRatPathElems.push(map.append("path").
 	attr("d", "M" + curpath[0] + "L" + curpath[1] + "z").
 			   attr("class", "spiny-rat-invis"));
+//			   attr("marker-end", "url(#head)"));
 }
