bc781bd3ed26d21e69dbddb33d640199d378c751
cshabsin <cshabsin@gmail.com>
2014-01-11 10:21:07 -0500

    Throw that in a .ready in case that helps. But it doesn't seem to...

--- hexmap-test.js
+++ hexmap-test.js
@@ -1,14 +1,16 @@
-var $map = $("#map");
-var $svg = $("<svg height=3000 width=3000/>");
-$map.append($svg);
-var $g = $("<g/>");
-$g.attr("class", "map-anchor-group");
-$g.attr("transform", "translate(10, 10)");
-$svg.append($g);
-var $path = $("<path/>");
-$path.attr("class", "map-mesh");
-$g.append($path);
-
-var myMap = new hexmap.Hexmap(4, 4, 10);
-
-$path.attr("d", myMap.gridMesh());
+$( document ).ready(function() {
+    var $map = $("#map");
+    var $svg = $("<svg height=3000 width=3000/>");
+    $map.append($svg);
+    var $g = $("<g/>");
+    $g.attr("class", "map-anchor-group");
+    $g.attr("transform", "translate(10, 10)");
+    $svg.append($g);
+    var $path = $("<path/>");
+    $path.attr("class", "map-mesh");
+    $g.append($path);
+    
+    var myMap = new hexmap.Hexmap(4, 4, 10);
+    
+    $path.attr("d", myMap.gridMesh());
+});
