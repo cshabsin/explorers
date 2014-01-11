0b40cd446e7dc4261686e6e4ddb01f105bfefd9f
cshabsin <cshabsin@gmail.com>
2014-01-11 13:32:35 -0500

    Clean this up a bit.

--- hexmap-test.js
+++ hexmap-test.js
@@ -1,16 +1,16 @@
-$( document ).ready(function() {
-    var $map = $("#map");
-    var $svg = $makeSVG("svg", {height:300, width:300});
-    $map.append($svg);
-    var $g = $makeSVG("g", {
-	"class": "map-anchor-group",
-	transform: "translate(10, 10)",
-    });
-    $svg.append($g);
-    var $path = $makeSVG("path", {"class": "map-mesh"});
-    $g.append($path);
-    
-    var myMap = new hexmap.Hexmap(4, 4, 10);
-    
-    $path.attr("d", myMap.gridMesh());
+var myMap = new hexmap.Hexmap(6, 5, 10);
+
+var $map = $("#map");
+var $svg = $makeSVG("svg", {height:300, width:300});
+$map.append($svg);
+var $g = $makeSVG("g", {
+    "class": "map-anchor-group",
+    transform: "translate(10, 10)",
 });
+$svg.append($g);
+var $path = $makeSVG("path", {
+    "class": "map-mesh",
+    d: myMap.gridMesh(),
+});
+$g.append($path);
+
