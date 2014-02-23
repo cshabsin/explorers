8752cd30a67b26d2addf14cd7aaeccee1d4d6456
Chris Shabsin <cshabsin@gmail.com>
2014-02-23 00:06:02 -0500

    New data entry.

--- data.js
+++ data.js
@@ -38,44 +38,87 @@ function systemHref(sys_short_name) {
 
 addSystem("Terschaps", 16, 12);
 addSystem("Mimmar Shari", 16, 13);
-addSystem("Mapiikhaar", 16, 14, href=systemHref("Mapiikhaar"));
+addSystem("Mapiikhaar", 16, 14, href=systemHref("Mapiikhaar"))
+    .setDescription(
+	"Pre-Imperial research station. We got data for it from Iraar Lar."
+    );
 addSystem("Iishmarmep", 16, 16);
 addSystem("Riimishkin", 16, 21);
 
 addSystem("Hasaggan", 17, 11);
 addSystem("Gipuurkhir", 17, 13);
 addSystem("Kuuhuurga", 17, 17);
-addSystem("Forquee", 17, 19, href=systemHref("Forquee"));
+addSystem("Forquee", 17, 19, href=systemHref("Forquee"))
+    .setDescription(
+	"&ldquo;Leave Forquee alone!&rdquo; Learned Karma techniques " +
+	"(e.g. purging Fury) from the aliens(Droyne?). Flying domed " +
+	"city. Under Virus attack when we left."
+    );
 addSystem("Kemkeaanguu", 17, 21);
 
-addSystem("Did", 18, 13, href=systemHref("Did"));
+addSystem("Did", 18, 13, href=systemHref("Did"))
+    .setDescription(
+	"Message to deliver from Imperial Office of Calendar " +
+	"Compliance. 10 MCr reward!"
+    );
 addSystem("Arlur", 18, 14);
-addSystem("Uure", 18, 18, href=systemHref("Uure"));
+addSystem("Uure", 18, 18, href=systemHref("Uure"))
+    .setDescription(
+	"Father's Lodge. Ancient crash site for Imperial archaeological " +
+	"support ship. Our first lesson on Karma, Madness, and Fury.<p>" +
+	"On our second pass through, Virus bombarded the archaeological " +
+	"site, and the Lodge initiated a solar flare to destroy the " +
+	"Virus fleet."
+    );
 addSystem("InmuuKi", 18, 20);
 
-addSystem("Irshuushdaar", 19, 15, href=systemHref("Irshuushdaar"));
+addSystem("Irshuushdaar", 19, 15, href=systemHref("Irshuushdaar"))
+    .setDescription(
+	"Where the space wreck was sent to jump to, presumably by the " +
+	"Marquis of Rohintash's fury infused ally."
+    );
 addSystem("Muukher", 19, 16);
-addSystem("Irdar Ga", 19, 17, href=systemHref("Irdar_Ga"));
-addSystem("Vlair", 19, 18, href=systemHref("Vlair"));
+addSystem("Irdar Ga", 19, 17, href=systemHref("Irdar_Ga"))
+    .setDescription(
+	"Ships that go here don't come back, because they're eaten " +
+	"by nanites. The nanites are building (have built?) something."
+    );
+addSystem("Vlair", 19, 18, href=systemHref("Vlair"))
+    .setDescription(
+	"First time in system: Storm, Omar Factors<p>" +
+	"Second time in system: Mine of madness and sarlacc maw"
+    );
 addSystem("Ziger", 19, 21);
 
 addSystem("Gier Iir", 20, 13);
 addSystem("Gimi Kuuid", 20, 17, href=systemHref("Gimi_Kuuid"))
     .setDescription(
 	"Underwater spherical displacement &mdash; signs of alternate 'madness' universe with dark changes (dictator vs. administrator, etc.).<p>" +
-	    "Gained hexagon key in a pawn shop here.<p>" +
-	    "Revolution underway on our way back through here on [date]. Virus is also in-system since [date].<p>" +
-	    "One of the planets hosting a Galactic Traveler office."
+	"Gained hexagon key in a pawn shop here.<p>" +
+	"Revolution underway on our way back through here on [date]. Virus is also in-system since [date].<p>" +
+	"One of the planets hosting a Galactic Traveler office."
     );
 addSystem("Garuu Uurges", 20, 18);
 addSystem("Daaruugka", 20, 19);
 
-addSystem("Girgulash", 21, 16, href=systemHref("Girgulash"));
-addSystem("Khida", 21, 18, href=systemHref("Khida")).setDescription(
-    "Stefan, Kyle Vesta, and Dr. Denmark are from here.<p>" +
-	"Dr. Denmark was kidnapped here, kicking off our group's association with him.<p>" +
-	"Khida Secundus Defensive Facility, Grandma Vesta, Ling Standard Products<p>" +
-	"The last time we were here, a 'bone' ship (Madness?) jumped in, and we helped destroy it."
+addSystem("Girgulash", 21, 16, href=systemHref("Girgulash"))
+    .setDescription(
+	"Trango is a moon of Girgulash, home of Trango shipyards, " +
+	"headed by Messorius Thraxton. Thraines, a city on Girgulash " +
+	"proper, hosted an Imperial Archive. <p>This is where we " +
+	"found the message for the Office of Calendar Compliance " +
+	"on Did.<p>First encounter with wireheads; they bombed a " +
+	"dome on Trango."
+    );
+addSystem("Khida", 21, 18, href=systemHref("Khida"))
+    .setDescription(
+	"Stefan, Kyle Vesta, and Dr. Denmark are from here.<p>" +
+	"Dr. Denmark was kidnapped here, kicking off our group's " +
+	"association with him.<p>" +
+	"Khida Secundus Defensive Facility, Grandma Vesta, Ling " +
+	"Standard Products<p>" +
+	"The last time we were here, a 'bone' ship (Madness?) jumped " +
+	"in, and we helped destroy it."
 );
 addSystem("Khui", 21, 19);
 
@@ -105,7 +148,11 @@ addSystem("Digapir", 25, 16);
 addSystem("Shiirla", 25, 19);
 addSystem("Dinkhaluurk", 25, 21);
 
-getHex(19, 13).setName("black hole").suppressPlanet = true;
+getHex(19, 13).setName("black hole")
+    .setDescription(
+	"Imperial observatory with Gertie was programmed not to " +
+	"be able to look at this parsec."
+    ).suppressPlanet = true;
 
 spinyRatPath = [];
 spinyRatPath.push(new path.PathSegment(hexes.Khida, [-10, -4],
